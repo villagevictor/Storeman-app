a9a2faea59531dd7d3667a92e228f70b268680db
villagevictor <villagevictor@users.noreply.github.com>
2026-08-21 12:08:39 +0300

    feat: publish Storeman ERP Web PWA

---
+++ web/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "storeman-v1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
